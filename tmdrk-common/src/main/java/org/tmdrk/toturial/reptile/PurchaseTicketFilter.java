d79ad6ead6c539c7a3df7fd7eabe48b40e77180c
Jie.Zhou <Jie.Zhou@g-town.com.cn>
2020-09-27 21:09:24 +0800

    刷票程序

--- tmdrk-common/src/main/java/org/tmdrk/toturial/reptile/PurchaseTicketFilter.java
+++ tmdrk-common/src/main/java/org/tmdrk/toturial/reptile/PurchaseTicketFilter.java
@@ -1,42 +1,69 @@
 package org.tmdrk.toturial.reptile;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.util.CollectionUtils;
 import org.tmdrk.toturial.common.util.SendEamilUtil;
-
-import javax.mail.MessagingException;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-import static org.tmdrk.toturial.reptile.HttpClientTest.ISO_8859_1;
-import static org.tmdrk.toturial.reptile.HttpClientTest.UTF_8;
-
 /**
  * @ClassName PurchaseTicketFilter
- * @Description TODO
+ * @Description 刷票程序
+ * 刷直达目的地或者最终经过目的地的短程火车票，并将合适的票发邮件提醒
  * @Author zhoujie
  * @Date 2020/9/26 20:49
  * @Version 1.0
  **/
 public class PurchaseTicketFilter {
     static String cookies="_uab_collina=160117018426887962014074; JSESSIONID=07B31A781F88FF35D72D8000E15818CB; RAIL_DEVICEID=XlNwgOJJhT5mZ0EwQ3rOQOw5Bobwvaxr-StHNqqyPuSh7HhI4-vS8jvJeNpCW0chJ6rphcTaD7JkCzsUwou2UiVTHWQKncBm49horB3oQ9ljbVkqID3ZsIW3R1VEdFotKbEIkLsW6pQILL6n6xZciCgPhCwk1kn1; RAIL_EXPIRATION=1601469495003; BIGipServerotn=2129133834.24610.0000; BIGipServerpassport=870842634.50215.0000; route=6f50b51faa11b987e576cdb301e545c4; _jc_save_fromStation=%u4E0A%u6D77%2CSHH; _jc_save_toStation=%u868C%u57E0%2CBBH; _jc_save_fromDate=2020-09-27; _jc_save_toDate=2020-09-27; _jc_save_wfdc_flag=dc";
+    /****** 去程 ******/
+    // 购票日期
     static String date = "2020-10-01";
+    // 理想票开始时间
+    static String startTime = "07:30";
+    // 理想票结束时间
+    static String endTime = "18:00";
+    // 购票url
     static List<String> urls = Arrays.asList(
+            //上海-蚌埠
             "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=BBH&purpose_codes=ADULT",
+            //上海-昆山
             "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=KNH&purpose_codes=ADULT",
+            //上海-苏州
             "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=SZH&purpose_codes=ADULT",
+            //上海-常州
             "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=CZH&purpose_codes=ADULT",
+            //上海-南京
             "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=NJH&purpose_codes=ADULT"
     );
+    /****** 返程 ******/
+//    // 购票日期
+//    static String date = "2020-10-07";
+//    // 理想票开始时间
+//    static String startTime = "07:30";
+//    // 理想票结束时间
+//    static String endTime = "17:00";
+//    // 购票url
+//    static List<String> urls = Arrays.asList(
+//            //蚌埠-上海
+//            "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=BBH&leftTicketDTO.to_station=SHH&purpose_codes=ADULT",
+//            //蚌埠-昆山
+//            "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=BBH&leftTicketDTO.to_station=KNH&purpose_codes=ADULT",
+//            //蚌埠-苏州
+//            "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=BBH&leftTicketDTO.to_station=SZH&purpose_codes=ADULT",
+//            //蚌埠-常州
+//            "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=BBH&leftTicketDTO.to_station=CZH&purpose_codes=ADULT",
+//            //蚌埠-南京
+//            "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date="+date+"&leftTicketDTO.from_station=BBH&leftTicketDTO.to_station=NJH&purpose_codes=ADULT"
+//    );
 
     public static void main(String[] args) throws InterruptedException {
         while (true) {
+            // 获取票信息
             List<String> tickets = new ArrayList<>();
             for (int i = 0; i < 5; i++) {
                 String json = HttpClientTest.doGet(urls.get(i), null, null, cookies);
@@ -45,8 +72,10 @@ public class PurchaseTicketFilter {
                 }
                 Thread.sleep(500);
             }
+            // 控制台打印匹配的票信息
             printTicket(tickets);
-            Thread.sleep(5000);
+            // 5秒一刷
+            Thread.sleep(10000);
         }
     }
 
@@ -60,27 +89,26 @@ public class PurchaseTicketFilter {
         for(int i=0;i<tickets.size();i++){
             list.clear();
             if(i==0){
+                // 目的地票信息单独处理，作为检索过滤基础信息。
                 Ticket ticket = JSON.parseObject(tickets.get(i), Ticket.class);
-                if(ticket != null){
-                    Ticket.TicketInfo ticketInfo = ticket.getData();
-                    ticketInfo.result.forEach(info->{
-                        if(info != null){
-                            String[] split = info.split("\\|");
-//                    System.out.println(split[3]+":"+split.length+"|"+split[30]+"|"+split[31]+"|"+split[32]);
-                            if(split[3].startsWith("G")||split[3].startsWith("D")){
-                                map.put(split[3],split);
-                                if(!Objects.equals(split[30],"无")){
-                                    addList(sb, list, ticketInfo, split, map.get(split[3]));
-                                    if("17:00".compareToIgnoreCase(split[8]) > 0 && "07:30".compareToIgnoreCase(split[8]) < 0){
-//                                    if("18:00".compareToIgnoreCase(split[8]) < 0 && "23:30".compareToIgnoreCase(split[8]) > 0){
-                                        addList(perfectSb, perfectList, ticketInfo, split, map.get(split[3]));
-                                    }
+                if(ticket == null) {
+                    throw new RuntimeException("目的地票信息不能为空");
+                }
+                Ticket.TicketInfo ticketInfo = ticket.getData();
+                ticketInfo.result.forEach(info->{
+                    if(info != null){
+                        String[] split = info.split("\\|");
+                        if(split[3].startsWith("G")||split[3].startsWith("D")){
+                            map.put(split[3],split);
+                            if(!Objects.equals(split[30],"无")){
+                                addList(sb, list, ticketInfo, split, map.get(split[3]));
+                                if(endTime.compareToIgnoreCase(split[8]) > 0 && startTime.compareToIgnoreCase(split[8]) < 0){
+                                    addList(perfectSb, perfectList, ticketInfo, split, map.get(split[3]));
                                 }
                             }
                         }
-                    });
-                }
-                list.forEach(System.out::println);
+                    }
+                });
             }else{
                 if(!(tickets.get(i).startsWith("{") || tickets.get(i).startsWith("["))){
                     System.out.println("error:"+tickets.get(i));
@@ -95,8 +123,7 @@ public class PurchaseTicketFilter {
                             if(map.containsKey(split[3])){
                                 if(!Objects.equals(split[30],"无")){
                                     addList(sb, list, ticketInfo, split, map.get(split[3]));
-                                    if("17:00".compareToIgnoreCase(split[8]) > 0 && "07:30".compareToIgnoreCase(split[8]) < 0){
-//                                    if("18:00".compareToIgnoreCase(split[8]) < 0 && "23:30".compareToIgnoreCase(split[8]) > 0){
+                                    if(startTime.compareToIgnoreCase(split[8]) < 0 && endTime.compareToIgnoreCase(split[8]) > 0){
                                         addList(perfectSb, perfectList, ticketInfo, split, map.get(split[3]));
                                     }
                                 }
@@ -104,15 +131,19 @@ public class PurchaseTicketFilter {
                         }
                     });
                 }
-                list.forEach(System.out::println);
             }
+            list.forEach(System.out::println);
             System.out.println("=======================================");
         }
-        System.out.println("+++++++++++++++++ 理想票 ++++++++++++++++");
+        System.out.println("+++++++++++++++++ 理想的票 ++++++++++++++++");
         perfectList.forEach(System.out::println);
+        System.out.println("----------------- 理想的票 ----------------");
+        // 如果有理想的票则发送邮件通知
         if(!CollectionUtils.isEmpty(perfectList)){
+            System.out.println("准备发送邮件...");
             try {
                 StringBuilder sbd = new StringBuilder();
+                sbd.append("车次   |").append("起始   |").append("结束  |").append("耗时  |").append("余票|").append("到目的地时间|").append("当前车次起始站").append("\n");
                 perfectList.forEach(per->{
                     sbd.append(per).append("\n");
                 });
@@ -121,7 +152,6 @@ public class PurchaseTicketFilter {
                 e.printStackTrace();
             }
         }
-        System.out.println("----------------- 理想票 ----------------");
     }
 
     private static void addList(StringBuilder sb, List<String> list, Ticket.TicketInfo ticketInfo, String[] split, String[] strings) {
